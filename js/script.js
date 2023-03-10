8f66d438b0efa475bf142fdec2e7bbb7b6dd81bf
ImamPSg <taufikimam03@gmail.com>
2023-03-10 22:23:12 +0700

    web belitang shope

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// togle kelas active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di click
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// proses onclick diluar humberger maka nav hilang
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+function message() {
+  var Name = document.getElementById("names");
+  var hp = document.getElementById("hp");
+  var msg = document.getElementById("msg");
+  const success = document.getElementById("success");
+  const danger = document.getElementById("danger");
+
+  if (Name.value === "" || hp.value === "" || msg.value === "") {
+    danger.style.display = "block";
+  } else {
+    setTimeout(() => {
+      Name.value = "";
+      hp.value = "";
+      msg.value = "";
+    }, 2000);
+
+    success.style.display = "block";
+  }
+
+  setTimeout(() => {
+    danger.style.display = "none";
+    success.style.display = "none";
+  }, 4000);
+}
